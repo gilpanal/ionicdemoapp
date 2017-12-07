abd65f938f2607a6d1a26f7cf99d1ff4c9ebad35
gilpanal <jose.m.gil.panal@accenture.com>
2017-12-07 18:59:22 +0100

    Add item functionality

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController } from 'ionic-angular';
 
 import { ItemDetailsPage } from '../item-details/item-details';
+import { AddItemPage } from '../add-item/add-item'
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
+  
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  public items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
 
@@ -31,4 +33,24 @@ export class ListPage {
       item: item
     });
   }
+
+  addItem(){
+    
+      let addModal = this.modalCtrl.create(AddItemPage);
+  
+      addModal.onDidDismiss((item) => {
+  
+            if(item){
+              this.saveItem(item);
+            }
+  
+      });
+  
+      addModal.present();
+  
+    }
+  
+    saveItem(item){
+      this.items.push(item);
+    }
 }
